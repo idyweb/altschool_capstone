d27cdab6e6c85a7c4e744f78705729646de065e4
Idongesit Inyang <inyangidongesit22@gmail.com>
2024-03-15 21:43:52 +0100

    improvements

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount('/static', StaticFiles(directory='static'), name='static')
+app.mount('./static', StaticFiles(directory='static'), name='static')
 
 app.include_router(router)
 app.include_router(url_router)
