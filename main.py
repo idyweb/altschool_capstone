f66b45b85b8af59aa4de6187a4a9a6b0a4367523
Idongesit Inyang <inyangidongesit22@gmail.com>
2024-03-15 21:53:38 +0100

    fixed minor issue

--- main.py
+++ main.py
@@ -25,12 +25,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount('./static', StaticFiles(directory='static'), name='static')
+app.mount('/static', StaticFiles(directory='./static'), name='static')
 
 app.include_router(router)
 app.include_router(url_router)
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="./templates")
 
 def get_db():
     db = SessionLocal()
